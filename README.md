09ccd2f006e2da70e23f9c88839655e9a7a5b564
whiskers75 <whiskers75@me.com>
2013-07-28 12:32:54 +0100

    Commit skeleton

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ whiskchat-server
 ================
 
 The server part to WhiskChat!
+Open source - licensed under the GPL v2 (see LICENSE)
+Permission given by CoinChat (coinchat.org) to derive from the work. CoinChat (c) admin@glados.cc
---
+++ server.js
@@ -0,0 +1,25 @@
+// WhiskChat Server! :D
+
+var io = require('socket.io').listen(80);
+var redis = require('redis');
+// For Heroku:
+io.configure(function () { 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
+});
+console.log('info - WhiskChat Server starting');
+console.log('info - Starting DB');
+if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var db = redis.createClient(rtg.port, rtg.hostname);
+    
+    db.auth(rtg.auth.split(":")[1]);
+} else {
+    var db = redis.createClient();
+}
+db.on('error', function(err) {
+    console.log('error - DB error: ' + err);
+});
+io.sockets.on('connection', function(socket) {
+    
+});
