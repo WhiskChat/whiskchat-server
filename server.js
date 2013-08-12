85256ec779bc4fc89393c4118b9cdee0a8d5ca20
whiskers75 <whiskers75@me.com>
2013-08-12 18:59:22 +0100

    Sync usernames with client - hotfix

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ function login(username, usersocket, sess) {
     if (users.indexOf(username) == -1) {
 	users.push(username);
     }
-    io.sockets.volatile.emit("online", {people: online});
+    io.sockets.volatile.emit("online", {people: online, array: users});
     if (sess) {
 	usersocket.emit('loggedin', {username: username, session: sess});
     }
