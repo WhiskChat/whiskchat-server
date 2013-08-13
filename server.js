79c42c617da2bc2b21771b67d5a7762d790adf22
cryptocoins <elacoin@glados.cc>
2013-08-13 17:46:02 +1000

    Update server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ function urlify(text) {
     }
     var urlRegex = /(https?:\/\/[^\s]+)/g;
     return text.replace(urlRegex, function(url) {
-        return '<a href="' + url + '">' + url + '</a>';
+        return '<a href="' + url + '" target="_blank">' + url + '</a>';
     });
 }
 function login(username, usersocket, sess) {
