25142821ee9a9df8a8893545160c79e2e1d27538
whiskers75 <whiskers75@me.com>
2013-08-16 19:19:12 +0100

    Add some rudimentary tests

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ var alphanumeric = /^[a-z0-9]+$/i;
 var muted = [];
 
 iottp.listen(process.env.PORT);
-
+if (process.argv[2] == "travisci") {
+    console.log('Travis CI mode active');
+    setTimeout(function() {
+	console.log('Auto-quitting after 10 seconds');
+	process.exit(0);
+    }, 10000);
+}
 io.configure(function () { 
     io.set("transports", ["xhr-polling"]); 
     io.set("polling duration", 10);
@@ -613,30 +619,30 @@ io.sockets.on('connection', function(socket) {
             });
         }
 	else {
-        db.get('users/' + tip.user, function(err, exists) {
-            if (exists) {
-                db.get('users/' + socket.user + '/balance', function(err, bal1) {
-                    db.get('users/' + tip.user + '/balance', function(err, bal2) {
-                        if ((Number(tip.tip) < bal1 || Number(tip.tip) == bal1) && Number(tip.tip) > 0 && tip.user != socket.user && muted.indexOf(socket.user) == -1) {
-                            db.set('users/' + socket.user + '/balance', Number(bal1) - Number(tip.tip), redis.print);
-                            db.set('users/' + tip.user + '/balance', Number(bal2) + Number(tip.tip), redis.print);
-                            sockets.forEach(function(cs) {
-                                cs.emit('tip', {room: tip.room, target: stripHTML(tip.user), amount: Number(tip.tip), message: tip.message, user: socket.user, timestamp: Date.now()});
-                                if (cs.user == socket.user) {
-                                    cs.emit('balance', {balance: Number(bal1) - Number(tip.tip)});
-                                }
-                                if (cs.user == tip.user) {
-                                    cs.emit('balance', {balance: Number(bal2) + Number(tip.tip)});
-                                }
-                            });
-                        }
-                        else {
-                            socket.emit('message', {type: "alert-error", message: "Your current balance is " + bal1 + " mBTC. Tip: " + tip.tip + "mBTC. Tip failed - you might not have enough, you may be muted or you are tipping yourself."});
-                        }
+            db.get('users/' + tip.user, function(err, exists) {
+		if (exists) {
+                    db.get('users/' + socket.user + '/balance', function(err, bal1) {
+			db.get('users/' + tip.user + '/balance', function(err, bal2) {
+                            if ((Number(tip.tip) < bal1 || Number(tip.tip) == bal1) && Number(tip.tip) > 0 && tip.user != socket.user && muted.indexOf(socket.user) == -1) {
+				db.set('users/' + socket.user + '/balance', Number(bal1) - Number(tip.tip), redis.print);
+				db.set('users/' + tip.user + '/balance', Number(bal2) + Number(tip.tip), redis.print);
+				sockets.forEach(function(cs) {
+                                    cs.emit('tip', {room: tip.room, target: stripHTML(tip.user), amount: Number(tip.tip), message: tip.message, user: socket.user, timestamp: Date.now()});
+                                    if (cs.user == socket.user) {
+					cs.emit('balance', {balance: Number(bal1) - Number(tip.tip)});
+                                    }
+                                    if (cs.user == tip.user) {
+					cs.emit('balance', {balance: Number(bal2) + Number(tip.tip)});
+                                    }
+				});
+                            }
+                            else {
+				socket.emit('message', {type: "alert-error", message: "Your current balance is " + bal1 + " mBTC. Tip: " + tip.tip + "mBTC. Tip failed - you might not have enough, you may be muted or you are tipping yourself."});
+                            }
+			});
                     });
-                });
-            }
-	});
+		}
+	    });
 	}
     });
     socket.on('getbalance', function() {
