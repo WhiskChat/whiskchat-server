a1bd3f2f206534ebc453c928d8c349b207fa829a
whiskers75 <whiskers75@me.com>
2013-08-28 08:51:45 +0100

    Donations increase rep

--- server.js
+++ server.js
@@ -767,13 +767,15 @@ io.sockets.on('connection', function(socket) {
         }
 	else {
 	    if (tip.user == "donate") {
-                        db.get('users/' + socket.user + '/balance', function(err, bal1) {
+                db.get('users/' + socket.user + '/balance', function(err, bal1) {
+		    db.get('users/' + socket.user + '/rep', function(err, rep1) {
                             db.get('system/donated', function(err, bal2) {
                                 if ((Number(tip.tip) < bal1 || Number(tip.tip) == bal1) && Number(tip.tip) > 0 && tip.user != socket.user && muted.indexOf(socket.user) == -1) {
                                     db.set('users/' + socket.user + '/balance', Number(bal1) - Number(tip.tip), redis.print);
                                     db.set('system/donated', Number(bal2) + Number(tip.tip), redis.print);
+				    db.set('users/' + socket.user + '/rep', (Number(rep1) - (Number(tip.tip) / 2)), redis.print);
                                     sockets.forEach(function(cs) {
-                                        cs.emit('tip', {room: tip.room, target: 'the WhiskChat Server Payout Pool [' + (Number(bal2) + Number(tip.tip)) + ' mBTC]', amount: Number(tip.tip), message: tip.message, user: socket.user, timestamp: Date.now()});
+                                        cs.emit('tip', {room: tip.room, target: 'the WhiskChat Server Payout Pool [' + (Number(bal2) + Number(tip.tip)) + ' mBTC] (+<i class="icon-gift"></i> ' + socket.user + ' [' + (Number(rep1) - (Number(tip.tip) / 2)) + '])', amount: Number(tip.tip), message: tip.message, user: socket.user, timestamp: Date.now()});
 					if (cs.user == socket.user) {
 					    socket.emit('balance', {balance: Number(bal1) - Number(tip.tip)});
 					}
@@ -783,7 +785,8 @@ io.sockets.on('connection', function(socket) {
                                     socket.emit('message', {type: "alert-error", message: "Your current balance is " + bal1 + " mBTC. Tip: " + tip.tip + "mBTC. Tip failed - you might not have enough, you may be muted or you are tipping yourself."});
                                 }
                             });
-                        });
+		    });
+		});
 	    }
             else {
             db.get('users/' + tip.user, function(err, exists) {
