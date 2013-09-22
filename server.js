a466f25e9d63c88d581cefb1236689070453ab30
whiskers75 <whiskers75@me.com>
2013-09-22 17:17:36 +0100

    Better earning algo

--- server.js
+++ server.js
@@ -526,7 +526,7 @@ function calculateEarns(user, socket, rep) {
         rep = 250;
     }
     if (rnd > socket.stage) {
-        socket.stage = socket.stage + ((rep / 10) * 0.002);
+        socket.stage = socket.stage + ((rep / 10) * 0.01);
         return null;
     }
     if (socket.rep < 5) { // Unwhitelisted!
@@ -535,7 +535,7 @@ function calculateEarns(user, socket, rep) {
     if (payoutbal < 0.01) {
         return null;
     }
-    socket.stage = ((rep / 10) * 0.002);
+    socket.stage = ((rep / 10) * 0.01);
     payoutbal = payoutbal - Number(((Math.log(rep) / 60)*Math.random()*1.75).toFixed(2));
     return Number(((Math.log(rep) / 60)*Math.random()*1.75).toFixed(2));
 }
