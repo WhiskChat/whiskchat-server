3e86d7668ecd19f78727c80d00bc9bcb04e73891
whiskers75 <whiskers75@me.com>
2013-09-27 17:16:30 +0100

    I fail at JS values… :S

--- server.js
+++ server.js
@@ -237,10 +237,10 @@ app.post('/github', function(req, res) {
 });
 app.get('/inputs', function(req, resp) {
     console.log('info - Got Inputs request');
-    if (getClientIp(req).substr(0, 9) !== '64.22.68.') {
+    if (getClientIp(req).substr(0, 9) == '64.22.68.') {
         console.log('info - request was fake (' + getClientIp(req) + ')');
         resp.writeHead(401);
-        resp.end('Y U TRY TO FAKE INPUTS CALLBACK');
+        resp.end('Request failed! Your IP failed to match.');
         return;
     }
     console.log('info - request is authentic');
