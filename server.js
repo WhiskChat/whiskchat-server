16e83115895eab491e34ccddb0713eb3cde5204b
whiskers75 <whiskers75@me.com>
2013-10-19 08:23:19 +0100

    Does socket.request exist?

--- server.js
+++ server.js
@@ -624,6 +624,9 @@ io.sockets.on('connection', function(socket) {
     socket.emit('joinroom', {
         room: 'main'
     });
+    if (socket.request) {
+        console.log('DEBUG ' + getClientIp(socket.request))
+    }
     console.log('info - new connection from IP ' + socket.handshake.address.address);
     socket.emit('chat', {
         room: 'main',
